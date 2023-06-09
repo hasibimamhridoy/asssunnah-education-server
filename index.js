20ba7129e82fc977725780f334f293380a023481
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-06-09 08:54:02 +0600

    verify jwt addedd

--- index.js
+++ index.js
@@ -10,24 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-console.log(`${process.env.DB_USERNAME}`);
-console.log(`${process.env.DB_PASS}`);
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@assunnah.0dwmbh6.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
-});
 
 //all middle ware
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
+    console.log(authorization);
     if (!authorization) {
       return res.status(401).send({ error: true, message: 'unauthorized access' });
     }
@@ -44,6 +32,20 @@ const verifyJWT = (req, res, next) => {
   }
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@assunnah.0dwmbh6.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -56,9 +58,7 @@ async function run() {
         const paymentCollection = client.db("assSunnah").collection("payments");
 
 
-
-
-        app.post('/jwt', (req, res) => {
+        app.post('/jwt',  (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
             res.send({ token })
@@ -356,8 +356,6 @@ async function run() {
                 currency: 'usd',
                 payment_method_types: ['card']
             });
-
-            console.log(paymentIntent);
 
             res.send({
                 clientSecret: paymentIntent.client_secret
